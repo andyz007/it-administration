d08a8e3e529ae9d3b0913de3245d49f0f634ab2b
Andreas Schulz <andreas.schulz@amplicade.com>
2018-12-18 16:39:38 +0100

    Added sendIP script

---
+++ sendIP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# check and send ip address to email
+ 
+MYIP="$(ifconfig eno1 | grep 'inet'| awk '{print $2}' | cut -d ':' -f 2)";
+PUBLICIP="$(curl https://ipinfo.io/ip)"
+TIME="$(date)";
+ 
+LASTIPFILE='/home/amplicade/.last_ip_addr';
+PUBLICIPFILE='/home/amplicade/.last_publicip_addr';
+LASTIP="$(cat "${LASTIPFILE}")";
+LASTPUBLICIP="$(cat "${PUBLICIPFILE}")";
+ 
+if [[ "${PUBLICIP}" != "${LASTPUBLICIP}" ]]
+then
+        echo "New public IP = '${PUBLICIP}'"
+        echo "sending email.."
+        echo -e "Hello\n\nTimestamp = ${TIME}\nPublicIP = ${PUBLICIP}\n\nBye" | \
+                /usr/bin/mail -s "[INFO] New IP" apps@amplicade.com;
+        echo "${PUBLICIP}" > "${PUBLICIPFILE}";
+else
+        echo "no public IP change!"
+fi
+
+crontab -e 
